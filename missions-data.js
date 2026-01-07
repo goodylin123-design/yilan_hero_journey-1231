e87d00c08e9f1f9919e88513233f095635a4fefc
goodylin123-design <goodylin123@gmail.com>
2026-01-07 17:52:06 +0800

    Add files via upload

---
+++ missions-data.js
@@ -0,0 +1,97 @@
+// 任務資料定義（10 關）
+// 之後可在這裡擴充多語文案與更多欄位
+
+window.MISSIONS = [
+  {
+    id: 'wave',
+    order: 1,
+    key: 'mission1',
+    title: '海風中的呢喃',
+    locationName: '蜜月灣',
+    lat: 24.938746,
+    lng: 121.889067
+  },
+  {
+    id: 'mountain-breeze',
+    order: 2,
+    key: 'mission2',
+    title: '山風掃帚',
+    locationName: '礁溪櫻花陵園',
+    lat: 24.835611,
+    lng: 121.760944
+  },
+  {
+    id: 'dune-footprints',
+    order: 3,
+    key: 'mission3',
+    title: '沙丘上的腳印',
+    locationName: '三貂水',
+    lat: 24.702000,
+    lng: 121.836300
+  },
+  {
+    id: 'earth-embrace',
+    order: 4,
+    key: 'mission4',
+    title: '大地的擁抱',
+    locationName: '壯圍沙丘生態園區',
+    lat: 24.744176,
+    lng: 121.822689
+  },
+  {
+    id: 'star-wish',
+    order: 5,
+    key: 'mission5',
+    title: '星空下的祈願',
+    locationName: '榕樹公園',
+    lat: 24.767120,
+    lng: 121.812450
+  },
+  {
+    id: 'wind-voices',
+    order: 6,
+    key: 'mission6',
+    title: '風中的聲音',
+    locationName: '五十二甲溼地',
+    lat: 24.685900,
+    lng: 121.790200
+  },
+  {
+    id: 'autumn-leaf',
+    order: 7,
+    key: 'mission7',
+    title: '拾起一片落葉',
+    locationName: '傳藝中心',
+    lat: 24.686596,
+    lng: 121.824067
+  },
+  {
+    id: 'river-reflection',
+    order: 8,
+    key: 'mission8',
+    title: '河流中的倒影',
+    locationName: '利澤簡',
+    lat: 24.673500,
+    lng: 121.818200
+  },
+  {
+    id: 'sea-rhythm',
+    order: 9,
+    key: 'mission9',
+    title: '拍打海浪的節奏',
+    locationName: '無尾港',
+    lat: 24.657300,
+    lng: 121.835700
+  },
+  {
+    id: 'farewell-ritual',
+    order: 10,
+    key: 'mission10',
+    title: '自然中的告別儀式',
+    locationName: '內埤情人灣',
+    lat: 24.582600,
+    lng: 121.866000
+  }
+];
+
+
